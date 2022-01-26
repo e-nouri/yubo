21c037a2910025675b9a081a289291959c537ba1
Nouri NOURI <nouri@ideel.io>
2022-01-26 19:41:09 +0100

    update the port to 8000

--- backend-yubo-safety/src/index.ts
+++ backend-yubo-safety/src/index.ts
@@ -12,7 +12,7 @@ import http from 'http';
 import { logger } from '@config/logger';
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '8000';
 
 async function startServer() {
   const app = createServer();
@@ -24,6 +24,7 @@ async function startServer() {
   });
 
   const signalTraps: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+
   signalTraps.forEach((type) => {
     process.once(type, async () => {
       logger.info(`process.once ${type}`);
